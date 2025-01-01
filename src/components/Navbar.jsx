1575e79357515698231d215f29958440e29bbe32
Eithel Gonzalez <eigonzalez@Mac-mini-de-Eithel.local>
2024-12-31 23:39:27 -0300

    fix word navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ function Navbar() {
             </li>
             <li>
               <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 md:p-0 md:dark:hover:text-green-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-green-400 md:dark:hover:bg-transparent dark:border-gray-700">
-                Services
+                Projects
               </a>
             </li>
             <li>
