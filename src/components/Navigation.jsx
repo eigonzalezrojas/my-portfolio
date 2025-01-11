cf6e65d910456f1e3e35b4c43d2444470d022188
Eithel Gonzalez <eigonzalez@Mac-mini-de-Eithel.local>
2025-01-10 23:30:41 -0300

    add hover effect navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../../src/App.css"
 
 function Navigation() {
   const [darkMode, setDarkMode] = useState(false);
@@ -9,25 +10,49 @@ function Navigation() {
   };
 
   return (
-    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-gray-800 dark:bg-gray-900 p-4 rounded-full shadow-2xl border-4 border-gray-300 dark:border-gray-700">
-      <div className="flex items-center space-x-4">
-        <a href="/" className="text-gray-300 hover:text-green-400 transition" aria-label="Home">
-          <i className="fas fa-home text-xl"></i>
+    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-gray-800 dark:bg-gray-900 p-2 rounded-full shadow-lg border-2 border-gray-300 dark:border-gray-700">
+      <div className="flex items-center space-x-2">
+        <a
+          href="/"
+          className="text-gray-300 hover:text-green-400 transition hover:scale-125"
+          aria-label="Home"
+        >
+          <i className="fas fa-home text-lg"></i>
         </a>
-        <a href="https://github.com/eigonzalezrojas" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-green-400 transition" aria-label="GitHub">
-          <i className="fab fa-github text-xl"></i>
+        <a
+          href="https://github.com/eigonzalezrojas"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 hover:text-green-400 transition hover:scale-125"
+          aria-label="GitHub"
+        >
+          <i className="fab fa-github text-lg"></i>
         </a>
-        <a href="https://www.linkedin.com/in/eigonzalezrojas/" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-green-400 transition" aria-label="LinkedIn">
-          <i className="fab fa-linkedin text-xl"></i>
+        <a
+          href="https://www.linkedin.com/in/eigonzalezrojas/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 hover:text-green-400 transition hover:scale-125"
+          aria-label="LinkedIn"
+        >
+          <i className="fab fa-linkedin text-lg"></i>
         </a>
-        <a href="mailto:eithelgonzalezrojas@gmail.com" className="text-gray-300 hover:text-green-400 transition" aria-label="Email">
-          <i className="fas fa-envelope text-xl"></i>
+        <a
+          href="mailto:eithelgonzalezrojas@gmail.com"
+          className="text-gray-300 hover:text-green-400 transition hover:scale-125"
+          aria-label="Email"
+        >
+          <i className="fas fa-envelope text-lg"></i>
         </a>
-        <button onClick={toggleTheme} className="text-gray-300 hover:text-green-400 transition focus:outline-none" aria-label="Toggle Theme">
+        <button
+          onClick={toggleTheme}
+          className="text-gray-300 hover:text-green-400 transition hover:scale-125 focus:outline-none"
+          aria-label="Toggle Theme"
+        >
           {darkMode ? (
-            <i className="fas fa-sun text-xl"></i>
+            <i className="fas fa-sun text-lg"></i>
           ) : (
-            <i className="fas fa-moon text-xl"></i>
+            <i className="fas fa-moon text-lg"></i>
           )}
         </button>
       </div>
