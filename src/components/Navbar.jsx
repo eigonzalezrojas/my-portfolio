f85b3eccd7f6bf18846762a35d3116de5f92bce0
Eithel Gonzalez <eigonzalez@Mac-mini-de-Eithel.local>
2025-01-13 23:06:58 -0300

    add cv document

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,10 +17,9 @@ function Navbar() {
         </a>
 
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">          
-          <button type="button" className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
+          <a href="/CV-EithelGonzalez.pdf" download="CV-EithelGonzalez.pdf" className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
             Download CV
-          </button>
-          
+          </a>
           <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-green-300" aria-controls="navbar-sticky" aria-expanded={isMenuOpen}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" >
